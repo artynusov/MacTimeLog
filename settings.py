5338c35687dedb241896b543057cef89a68cb607
Artem Yunusov <nedrlab@gmail.com>
2013-08-04 18:15:10 +0400

    Minor settings refactoring

--- settings.py
+++ settings.py
@@ -6,20 +6,24 @@
 #
 
 import os
+import logging
 
 from Foundation import *
+
 from durus.file_storage import FileStorage
 from durus.connection import Connection
 
 
-def getSettingsFolder(appName="MacTimeLog"):
+def getSettingsFolder(appName):
     """Return settings folder"""
     paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory,
             NSUserDomainMask, True)
     basePath = (len(paths) > 0 and paths[0]) or NSTemporaryDirectory()
     fullPath = basePath.stringByAppendingPathComponent_(appName)
+
     if not os.path.exists(fullPath):
-        os.mkdir(fullPath)
+        os.makedirs(fullPath)
+
     return fullPath
 
 
@@ -28,28 +32,49 @@ class _Settings(object):
 
     _appName = "MacTimeLog"
 
-    _defaultSettings = {
-        "dateFormat": "%m-%d-%Y %H:%M",
-        "timeFormat": "%H:%M",
-        "logDateTimeFormat": "at %H:%M",
-        "workEndTime": "06:00",
-        "workDayLength": 3600*8,
-        "timerInterval": 1,
-        "showWorkTill": False,
-        "showDateTime": False,
-        "logPath": "%s/%s" % (getSettingsFolder(_appName), "log.txt"),
-        "projectsDataPath": "%s/%s" % (getSettingsFolder(_appName), "projects"),
-        "slackingDataPath": "%s/%s" % (getSettingsFolder(_appName), "slacking"),
-        "logEditCommand": "open -a TextEdit \"%s\"",
-        "projectSeparator": "::",
-        "selectedProject": "Default",
-        "startPlaceholder": "__start__",
-        "showNotification": False,
-        "notificationTime": 40,
-        "notificationRepeatTime": 10,
-        "soundOnNotification": False,
-        "showHelpMessageOnStart": True
-    }
+    _defaultSettings = dict(
+
+        dateFormat="%m-%d-%Y %H:%M",
+
+        timeFormat="%H:%M",
+
+        logDateTimeFormat="at %H:%M",
+
+        workEndTime="06:00",
+
+        workDayLength=3600 * 8,
+
+        timerInterval=1,
+
+        showWorkTill=False,
+
+        showDateTime=False,
+
+        logPath="{0}/{1}".format(getSettingsFolder(_appName), "log.txt"),
+
+        projectsDataPath="{0}/{1}".format(getSettingsFolder(_appName), "projects"),
+
+        slackingDataPath="{0}/{1}".format(getSettingsFolder(_appName), "slacking"),
+
+        logEditCommand="open -a TextEdit \"%s\"",
+
+        projectSeparator="::",
+
+        selectedProject="Default",
+
+        startPlaceholder="__start__",
+
+        showNotification=False,
+
+        notificationTime=40,
+
+        notificationRepeatTime=10,
+
+        soundOnNotification=False,
+
+        showHelpMessageOnStart=True,
+
+    )
 
     _globalSettings = {}
 
@@ -58,14 +83,12 @@ class _Settings(object):
         self._conn = Connection(FileStorage(self._settingsFile))
         self._globalSettings = self._conn.get_root()
 
+    def __getattr__(self, key):
+        return self.get(key)
+
     def get(self, key):
         """Return setting value by key"""
-        if key in self._globalSettings:
-            return self._globalSettings[key]
-        elif key in self._defaultSettings:
-            return self._defaultSettings[key]
-        else:
-            pass
+        return self._globalSettings.get(key, self._defaultSettings[key])
 
     def set(self, key, value):
         """Set setting value by key"""
